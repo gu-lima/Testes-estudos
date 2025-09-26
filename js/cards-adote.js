56511b7aadab786f3318ee8ef6c17bc9966efbab
Luegiu Yuusha-sama <cassianofpereira@gmail.com>
2025-09-26 02:08:02 -0300

    sim

--- js/cards-adote.js
+++ js/cards-adote.js
@@ -16,7 +16,7 @@ async function generateCard() {
         let card = document.createElement("div");
 
         let img = document.createElement("img");
-        img.src = "../../assets/dog1.jpg";
+        img.src = "../../assets/dog-1.jpg";
         img.alt = `Foto do ${element.name}`;
         card.className = "card";
 

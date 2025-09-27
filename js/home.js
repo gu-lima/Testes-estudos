df29664d8a389d8179c3c4e28121833995372e25
Luegiu Yuusha-sama <cassianofpereira@gmail.com>
2025-09-27 11:39:21 -0300

    Possivel versão final

--- js/home.js
+++ js/home.js
@@ -1,9 +1,17 @@
 async function logged() {
     let anchor = document.getElementById("account-anchor");
+    let anchor2 = document.getElementById("account-anchor2");
     let session = JSON.parse(sessionStorage.getItem("session"));
     
-    anchor.textContent = session ? `${session.name.split(" ")[0]}` : "Cadastre-se";
-    console.log(session);
+    if (session){
+        anchor.textContent = `${session.name.split(" ")[0]}`;
+        anchor2.textContent = `${session.name.split(" ")[0]}`;
+        anchor.href = "./pages/perfil";
+        anchor2.href = "./pages/perfil"
+    }
+
+    anchor.textContent = "Cadastre-se";
+    anchor2.textContent = "Cadastre-se";
 }
 
 document.addEventListener("DOMContentLoaded", async () => {

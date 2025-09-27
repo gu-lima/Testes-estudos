c9f20072ea3fec964680cd07cb527d0981494465
Luegiu Yuusha-sama <cassianofpereira@gmail.com>
2025-09-27 11:16:02 -0300

    aaa

--- js/home.js
+++ js/home.js
@@ -1,6 +1,6 @@
 async function logged() {
     let anchor = document.getElementById("account-anchor");
-    let session = sessionStorage.getItem("session");
+    let session = JSON.parse(sessionStorage.getItem("session"));
     
     console.log(session);
 }

3bdec96508b20950423307da4448e1f575141d24
Luegiu Yuusha-sama <cassianofpereira@gmail.com>
2025-09-27 11:23:29 -0300

    Ainda em desenvolvimento, versionando para teste

--- js/home.js
+++ js/home.js
@@ -2,6 +2,7 @@ async function logged() {
     let anchor = document.getElementById("account-anchor");
     let session = JSON.parse(sessionStorage.getItem("session"));
     
+    anchor.textContent = session ? `${session.name.split(" ")[0]}` : "Cadastre-se";
     console.log(session);
 }
 
